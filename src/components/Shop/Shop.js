5e58ace5525f98f31467c1dea44704f94a9255a0
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-03-22 19:43:23 +0600

    font awsame added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,6 +3,8 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import Product from '../Product/Product';
 import './Shop.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -39,8 +41,12 @@ const Shop = () => {
                     <p>Grand Total: $5 </p>
                 </div>
                 <div className="button">
-                    <button className='clear-btn'>Clear Cart</button>
-                    <button className='review-btn'>Review Order</button>
+                    <button className='clear-btn'>Clear Cart
+                    <FontAwesomeIcon icon={faTrash} className='icon'></FontAwesomeIcon>
+                    </button>
+                    <button className='review-btn'>Review Order
+                    <FontAwesomeIcon icon={faAngleRight} className='icon'></FontAwesomeIcon>
+                    </button>
                 </div>
             </div>
         </div>
